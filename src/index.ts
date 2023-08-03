35fac337218a1c867baf747da5fcd205651faab8
aymene01 <aymene01@hotmail.fr>
2023-08-03 15:24:46 +0200

    remove code from index

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,2 @@
 export * from './Functor'
 export * from './Monad'
-
-const sumAll = (xs: number[]): number => {
-  if (xs.length === 0) {
-    return 0
-  }
-  const [head, ...rest] = xs
-
-  return head + sumAll(rest)
-}
-
-const sumAll2 = (xs: number[]): number => (xs.length === 0 ? 0 : xs[0] + sumAll2(xs.slice(1)))
-
-console.log(sumAll2([1, 8, 9]))
